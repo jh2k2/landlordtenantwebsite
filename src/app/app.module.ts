ba6b599341595abd94d20c808bf1011f5be2b1ca
Dylan Feng <dylanfeng@Dylans-MacBook-Pro.local>
2020-06-17 01:04:30 -0700

    Home Page Design Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
 import { BotFooterComponent } from './bot-footer/bot-footer.component';
 import { HeroComponent } from './hero/hero.component';
-import { GalleryComponent } from './gallery/gallery.component';
+import { LocationComponent } from './location/location.component';
 import { ForTlComponent } from './for-tl/for-tl.component';
 import { TextVariationComponent } from './text-variation/text-variation.component';
 
@@ -16,7 +16,7 @@ import { TextVariationComponent } from './text-variation/text-variation.componen
     TopNavBarComponent,
     BotFooterComponent,
     HeroComponent,
-    GalleryComponent,
+    LocationComponent,
     ForTlComponent,
     TextVariationComponent,
   ],
--- src/app/location/location.component.spec.ts
+++ src/app/location/location.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { GalleryComponent } from './gallery.component';
+import { LocationComponent } from './location.component';
 
-describe('GalleryComponent', () => {
-  let component: GalleryComponent;
-  let fixture: ComponentFixture<GalleryComponent>;
+describe('LocationComponent', () => {
+  let component: LocationComponent;
+  let fixture: ComponentFixture<LocationComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GalleryComponent ]
+      declarations: [ LocationComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(GalleryComponent);
+    fixture = TestBed.createComponent(LocationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
